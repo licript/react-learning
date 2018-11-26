45b942f14a74e610dfde6bfb9e9555328044c6e3
liyang <liyang@billionss.com>
2018-11-26 21:49:11 +0800

    add more

--- src/HOC/NewComponent.js
+++ src/HOC/NewComponent.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 
-const NewComponent = (WrapComponent) => {
+const NewComponent = (WrapComponent,data) => {
     class HOC extends Component {
+        componentWillMount() {
+            //操作 data 通过给 WrapComponent 
+            //传入 Props 让 WrapCom 变成拥有不同数据的组件
+        }
         render() {
             return(
                 <div>
